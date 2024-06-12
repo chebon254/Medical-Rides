652a0c4d0fffb22d2c228bd223fb40c8f1cc26f4
bravin <bravinrutoh@gmail.com>
2024-06-11 23:15:43 -0400

    Protected the api routes

--- src/app/api/bookings/pending/route.ts
+++ src/app/api/bookings/pending/route.ts
@@ -1,9 +1,19 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { auth } from '@clerk/nextjs/server';
 
 const prisma = new PrismaClient();
 
 export async function GET() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: 'Error: No signed in user' },
+      { status: 401 },
+    );
+  }
+
   try {
     const bookings = await prisma.booking.findMany({
       where: { rideStatus: 'pending' }
